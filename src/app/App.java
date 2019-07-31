eb6874f9fa431f74407496a9733a07d62baaeb11
kazurego7 <kazurego7@gmail.com>
2019-07-31 20:31:11 +0900

    streamとクラスやインターフェースあそび

--- src/app/App.java
+++ src/app/App.java
@@ -1,7 +1,25 @@
 package app;
 
+import java.util.stream.*;
+import java.util.stream.IntStream;
+
+interface IHelloRepeater {
+    void repeatHelloAt(int i);
+}
+
+class HelloRepeater implements IHelloRepeater {
+    public void repeatHelloAt(int i) {
+        IntStream.range(0, i).forEach((k) -> System.out.println(String.format("hello %05d", k)));
+        return;
+    }
+}
+
 class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello World!");
+        HelloRepeater speaker = new HelloRepeater();
+        speaker.repeatHelloAt(6);
+
+        Stream<Integer> stream_int = IntStream.range(0, 4).boxed();
     }
 }
